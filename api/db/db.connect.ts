9a3b735397eea5337f90d775c73201111bfbb08f
whitedevil31 <magz3116@gmail.com>
2021-02-10 17:23:45 +0530

    refactor(db):Mongoclient

--- api/db/db.connect.ts
+++ api/db/db.connect.ts
@@ -1,16 +1,35 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
-    console.log(`mongoose connected to : ${connect.connection.host}`);
-  } catch (e) {
-    console.log(e);
+import { MongoClient } from "mongodb";
+import { errorType } from "../types/types";
+let client: MongoClient;
+export async function connectDB(): Promise<MongoClient> {
+  client = await MongoClient.connect(process.env.MONGO_URI || "", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ignoreUndefined: true,
+  });
+  console.log("✔️   Connected to Database");
+  return client;
+}
+export async function getClient(): Promise<MongoClient> {
+  if (!client) {
+    await connectDB();
   }
-};
-
+  return client;
+}
 export default connectDB;
+// const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     const connect = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`mongoose connected to : ${connect.connection.host}`);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// export default connectDB;
