ec2e822de91f0d8403480a5083bb793e153d0fb4
whitedevil31 <magz3116@gmail.com>
2021-02-20 21:41:08 +0530

    css recheck

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,12 +1,5 @@
 const { model } = require("mongoose");
 const tailwindcss = require("tailwindcss");
 module.exports = {
-  plugins: [
-    tailwindcss("./tailwind.config.js"),
-    require("autoprefixer"),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./src/*.tsx", "./public/index.html"],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
-  ],
+  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
 };
