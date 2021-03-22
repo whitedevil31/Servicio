c80bbc0dfd3479285112806d77ffc91c83e1c6d7
Kaushik Kumaran <kumarankaushik@gmail.com>
2021-03-22 18:30:35 +0400

    ✨ SignUp final

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Signup() {
   interface Coordinates {
@@ -49,179 +49,176 @@ function Signup() {
 
   return (
     <div>
-      <div className="mt-10 sm:mt-5 sm:mb-5 sm:ml-16">
-        <div className="md:grid md:grid-cols-2 md:gap-2">
-          <div className="md:col-span-1">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="shadow overflow-hidden sm:rounded-md ">
-                <div className="px-2 py-2 bg-white sm:p-6 border-r-2 border-indigo-400">
-                  <div className="grid grid-cols-6 gap-10">
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="first_name"
-                        className="block text-sm font-medium text-indigo-700"
-                      >
-                        Username
-                      </label>
+      <div className="flex float-left">
+        <img
+          className="h-full w-4/5 sm:h-full md:h-screen lg:w-96 lg:h-screen rounded-r-2xl"
+          src="https://images.unsplash.com/photo-1613608698681-47e441065aa0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          alt=""
+        ></img>
+        <div className="px-2 py-2 shadow-inner bg-blur-md bg-brightness-75 sm:p-6 rounded-xl -ml-72 mt-28 h-full w-72 text-white">
+            <div className="pt-12 text-3xl font-extrabold">Find your perfect Utility helper</div>
+            <div className="pt-12 text-5xl font-bold">In a single Click</div>
+            <div className="pt-5 text-xs">Already a member? <a className="font-bold" href="http://localhost:3000/login">Sign in</a>
+            </div>
+        </div>
 
-                      <input
-                        type="text"
-                        name="username"
-                        ref={register}
-                        className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2  rounded-md"
-                      ></input>
+      </div>
+      
+      <div className="float-right w-2/4 mt-10 sm:mt-3 sm:mb-3 sm:mr-28">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="shadow-2xl overflow-hidden sm:rounded-md ">
+            <div className="px-2 py-2 shadow-inner bg-white sm:p-6 border-r-2 border-indigo-400">
+              <label
+                htmlFor="first_name"
+                className="block text-2xl font-cursive text-indigo-900"
+              >
+                Get Started, Register now!
+              </label>
+              <div className="flex flex-row">
+                <div className="mt-10 float-left w-1/2 ml-3 mr-5">
+                  <label
+                    htmlFor="first_name"
+                    className="block text-sm font-medium text-indigo-700"
+                  >
+                    Username
+                  </label>
 
-                      <input
-                        type="text"
-                        name="email"
-                        ref={register}
-                        className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2  rounded-md"
-                      ></input>
-                    </div>
+                  <input
+                    type="text"
+                    name="username"
+                    ref={register}
+                    autoComplete="off"
+                    className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md transition duration-500 ease-in-out hover:shadow-inner"
+                  ></input>
+                </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label className="block text-sm font-medium text-indigo-700">
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        ref={register}
-                        className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2 rounded-md"
-                      ></input>
-                      <p className="mt-2 text-sm text-indigo-700">
-                        Use atleast 6-8 characters, include numbers and
-                        uppercase.
-                      </p>
-                    </div>
+                <div className="mt-10 float-right w-1/2 ml-3 mr-5">
+                  <label className="block text-sm font-medium text-indigo-700">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    ref={register}
+                    className="mt-1 h-10 focus:ring-indigo-500  focus:border-indigo-500 block w-full shadow-xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                  ></input>
+                  <p className="flex-col mt-2 text-xs text-indigo-400">
+                    Use atleast 6-8 characters, include numbers and uppercase.
+                  </p>
+                </div>
+              </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label className="block text-sm font-medium text-indigo-700">
-                        Age
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          type="number"
-                          name="age"
-                          ref={register}
-                          className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2 rounded-md"
-                        ></input>
-                      </div>
-                    </div>
+              <div className="flex flex-row ">
+                <div className="mt-7 float-left w-1/2 ml-3 mr-5">
+                  <label className="block text-sm font-medium text-indigo-700">
+                    Age
+                  </label>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium text-indigo-700"
-                      >
+                  <input
+                    type="number"
+                    name="age"
+                    autoComplete="off"
+                    ref={register}
+                    className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                  ></input>
+                </div>
+
+                <div className="mt-7 float-right w-1/2 ml-3 mr-5">
+                  <label className="block text-sm font-medium text-indigo-700">
+                    Gender
+                  </label>
+                  <div className="mt-1">
+                    <select
+                      className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                      // value={gender}
+                      ref={register}
+                      name="gender"
+                      // onChange={handleChangeGender}
+                    >
+                      <option value="none" selected hidden>
                         Gender
-                      </label>
-                      <div className="mt-1">
-                        <select
-                          className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2 rounded-md"
-                          // value={gender}
-                          ref={register}
-                          name="gender"
-                          // onChange={handleChangeGender}
-                        >
-                          <option value="none" selected hidden>
-                            Gender
-                          </option>
-                          <option className="select" value="Male">
-                            Male
-                          </option>
-                          <option className="select" value="Female">
-                            Female
-                          </option>
-                          <option className="select" value="Other">
-                            Other
-                          </option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium text-indigo-700"
-                      >
-                        Residence/Area
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          type="text"
-                          name="residence"
-                          id="area"
-                          ref={register}
-                          className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2  rounded-md"
-                        ></input>
-                      </div>
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3">
-                      <legend className="block text-sm font-medium text-indigo-700">
-                        Role
-                      </legend>
-                      <div className="mt-1 space-y-4">
-                        <select
-                          className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2 rounded-md"
-                          // value={role}
-                          ref={register}
-                          name="role"
-                          // onChange={handleChangeRole}
-                        >
-                          <option value="none" selected hidden>
-                            Role
-                          </option>
-                          <option className="select" value="Utility Helper">
-                            Utility Helper
-                          </option>
-                          <option className="select" value="User">
-                            User
-                          </option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-6">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium text-indigo-700"
-                      >
-                        About
-                      </label>
-                      <div className="mt-1">
-                        <textarea
-                          name="about"
-                          ref={register}
-                          className="mt-1 h-15 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-inner sm:text-sm border-gray-500 border-b-2 rounded-md"
-                          placeholder="Short Introduction"
-                        ></textarea>
-                      </div>
-                      <p className="mt-2 text-sm text-indigo-700">
-                        Let us know something about you.
-                      </p>
-                    </div>
-                    <div className="col-span-6 sm:col-span-6">
-                      <i className="fas fa-arrow-right">
-                        <input
-                          type="submit"
-                          className="mt-2 text-sm text-indigo-700"
-                        />
-                      </i>
-                      <button className="float-right">
-                        <input
-                          type="submit"
-                          className="border border-transparent text-base font-medium rounded-md text-indigo-50 bg-indigo-400 hover:bg-indigo-900 md:py-2 md:text-sm md:px-4 cursor-pointer"
-                        />
-                      </button>
-                    </div>
+                      </option>
+                      <option className="select" value="Male">
+                        Male
+                      </option>
+                      <option className="select" value="Female">
+                        Female
+                      </option>
+                      <option className="select" value="Other">
+                        Other
+                      </option>
+                    </select>
                   </div>
                 </div>
               </div>
-            </form>
+
+              <div className="flex flex-row ">
+                <div className="mt-10 float-left w-1/2 ml-3 mr-5">
+                  <label className="block text-sm font-medium text-indigo-700">
+                    Residence/Area
+                  </label>
+
+                  <input
+                    type="text"
+                    name="residence"
+                    id="area"
+                    ref={register}
+                    className="mt-1 h-10 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                  ></input>
+                </div>
+
+                <div className="mt-10 float-right w-1/2 ml-3 mr-5">
+                  <legend className="block text-sm font-medium text-indigo-700">
+                    Role
+                  </legend>
+                  <select
+                    className="mt-1 h-10 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                    // value={role}
+                    ref={register}
+                    name="role"
+                    // onChange={handleChangeRole}
+                  >
+                    <option value="none" selected hidden>
+                      Role
+                    </option>
+                    <option className="select" value="Utility Helper">
+                      Utility Helper
+                    </option>
+                    <option className="select" value="User">
+                      User
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="mt-10 w-full ml-3 mr-5">
+                <label
+                  htmlFor="about"
+                  className="block text-sm font-medium text-indigo-700"
+                >
+                  About
+                </label>
+                <div className="mt-1">
+                  <textarea
+                    name="about"
+                    ref={register}
+                    className="mt-1 h-15 px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-2xl sm:text-sm border-gray-500 border-b-2 rounded-md hover:shadow-inner"
+                    placeholder="Short Introduction"
+                  ></textarea>
+                </div>
+                <p className="mt-2 text-xs text-indigo-400">
+                  Let us know something about you.
+                </p>
+              </div>
+              <button className="float-right">
+                <input
+                  type="submit"
+                  className="border border-transparent text-base font-medium rounded-md text-indigo-50 bg-indigo-600 hover:bg-indigo-900 mb-3 md:py-2 md:text-sm md:px-4 cursor-pointer"
+                />
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
