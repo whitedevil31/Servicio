c4a8a856c48b2dafff0ac0a81d07183a474e055e
whitedevil31 <magz3116@gmail.com>
2021-03-22 07:55:33 +0530

    🗺️ init

---
+++ client/src/components/map.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Map = () => {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+    </div>
+    // <MapwithMarker
+    //   containerElement={<div style={{ height: "400px" }} />}
+    //   mapElement={<div style={{ height: "100%" }} />}
+    // />
+  );
+};
+export default Map;
