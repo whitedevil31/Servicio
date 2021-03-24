31ac81625290ab5da4e5a1ae43c74611c53a8726
Kaushik Kumaran <kumarankaushik@gmail.com>
2021-03-24 17:48:03 +0400

    right side assignee fix and new req posts space created

--- client/src/components/WorkerPostModal.tsx
+++ client/src/components/WorkerPostModal.tsx
@@ -4,6 +4,7 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 import axios, { AxiosRequestConfig } from "axios";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -23,44 +24,45 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export default function WorkerModal() {
+  const onSubmit = (data: any) => {
+    var selected = new Array();
+    var worker = document.getElementById("worker");
 
-    const onSubmit = (data: any) => {
-        var selected = new Array();
-        var worker = document.getElementById("worker");
-    
-        if (worker) {
-          var ticks = worker.getElementsByTagName("input");
-    
-          for (var i = 0; i < ticks.length; i++) {
-            if (ticks[i].checked) {
-              selected.push(ticks[i].value);
-            }
-          }
+    if (worker) {
+      var ticks = worker.getElementsByTagName("input");
+
+      for (var i = 0; i < ticks.length; i++) {
+        if (ticks[i].checked) {
+          selected.push(ticks[i].value);
         }
-    
-        console.log(selected);
-        console.log(data.pay)
-    
-        let config = {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-    
-        const workerData: any = {
-          ...data,
-          services: selected,
-        };
-    
-        axios
-          .post("http://localhost:5000/api/worker/post", workerData, config)
-          .then((response) => {
-            console.log(response);
-          });
-      };
+      }
+    }
 
-  const { register, handleSubmit } = useForm();
+    console.log(selected);
+    console.log(data.pay);
+
+    let config = {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const workerData: any = {
+      ...data,
+      services: selected,
+    };
+
+    axios
+      .post("http://localhost:5000/api/worker/post", workerData, config)
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  const { register, handleSubmit, errors } = useForm({
+    criteriaMode: "all",
+  });
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
@@ -75,7 +77,10 @@ export default function WorkerModal() {
   return (
     <div>
       <button type="button" onClick={handleOpen}>
-        <input className="ml-3 mt-5 px-2 py-2 w-96 rounded-2xl cursor-pointer" placeholder="Create a post"></input>
+        <input
+          className="ml-3 mt-5 px-2 py-2 w-96 rounded-2xl cursor-pointer"
+          placeholder="Create a post"
+        ></input>
       </button>
       <Modal
         aria-labelledby="transition-modal-title"
@@ -90,129 +95,135 @@ export default function WorkerModal() {
         }}
       >
         <Fade in={open}>
-          <div className={classes.paper}>
-            <div className="flex-row float-left px-5">
-              <img
-                className="h-8 w-8 mr-0 rounded-full"
-                src="https://images.unsplash.com/photo-1521710696740-c8144a7eaf88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-                alt=""
-              ></img>
-              <input
-                className="px-5 py-5 h-5 w-96 mt-4 rounded-md cursor-pointer "
-                type="text"
-                placeholder="Enter post"
-                id="delete-btn"
-              ></input>
+          <div className="flex-row float-left px-5">
+            <div
+              className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center"
+              id="overlay"
+            >
+              <div className="bg-gray-200 w-3/4 py-2 px-3 rounded shadow-xl text-gray-800">
+                <div className="flex justify-between items-center">
+                  <h4 className="text-lg mt-3 font-bold">Let's start here</h4>
+                  <button onClick={handleClose}>
+                    <svg
+                      className="h-6 w-6 cursor-pointer p-1 hover:bg-gray-300 rounded-full"
+                      id="close-modal"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
 
-              <div
-                className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center"
-                id="overlay"
-              >
-                <div className="bg-gray-200 max-w-2xl py-2 px-3 rounded shadow-xl text-gray-800">
-                  <div className="flex justify-between items-center">
-                    <h4 className="text-lg mt-3 font-bold">Let's start here</h4>
-                    <button onClick={handleClose}>
-                      <svg
-                        className="h-6 w-6 cursor-pointer p-1 hover:bg-gray-300 rounded-full"
-                        id="close-modal"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </button>
-                  </div>
-
-                  <form onSubmit={handleSubmit(onSubmit)} id="worker" >
+                <form onSubmit={handleSubmit(onSubmit)} id="worker">
                   <div className="flex flex-col mt-10">
                     <div>
                       <label className="mr-3 ml-5">Pay: </label>
                       <input
-                        type="number"
                         name="pay"
-                        ref={register}
+                        ref={register({
+                          required: "This input is required.",
+                          pattern: {
+                            value: /\d+/,
+                            message: "This input is number only.",
+                          },
+                          maxLength: {
+                            value: 10,
+                            message: "This input exceed maxLength.",
+                          },
+                        })}
                         autoComplete="off"
-                        className="h-4 px-3 py-3 rounded-md w-20"
+                        className="bg-transparent border-b-2 border-gray-500 shadow-inner h-4 px-3 py-3 rounded-md w-20"
                       ></input>
+                      <div className="text-red-600">
+                      <ErrorMessage
+                        errors={errors}
+                        name="pay"
+                        render={({ messages }) => {
+                          console.log("messages", messages);
+                          return messages
+                            ? Object.entries(
+                                messages
+                              ).map(([type, message]) => (
+                                <p key={type}>{message}</p>
+                              ))
+                            : null;
+                        }}
+                      />
+                      </div>
                     </div>
-
                     <div className="mt-5">
-                      <label className="mr-3 ml-5">Services: </label>
-                    </div>
+                        <label className="mr-3 ml-5">Services: </label>
+                      </div>
 
-                    <div className="mr-4 ml-4 mt-3 flex flex-row space-x-3">
-                      <input
-                        id="plumbing"
-                        ref={register}
-                        type="checkbox"
-                        value="plumbing"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      ></input>
-                      <label className="font-medium text-gray-700">
-                        Plumbing
-                      </label>
+                      <div className="mr-4 ml-4 mt-3 flex flex-row space-x-3">
+                        <input
+                          id="plumbing"
+                          ref={register}
+                          type="checkbox"
+                          value="plumbing"
+                          className="focus:ring-gray-500 h-4 w-4 text-gray-600 border-gray-400 rounded-full"
+                        ></input>
+                        <label className="font-medium mr-5 text-gray-700">
+                          Plumbing
+                        </label>
 
-                      <input
-                        id="plumbing"
-                        name="plumbing"
-                        ref={register}
-                        type="checkbox"
-                        value="Driver"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      ></input>
-                      <label className="font-medium text-gray-700">
-                        Driver
-                      </label>
-                      <input
-                        id="plumbing"
-                        name="plumbing"
-                        ref={register}
-                        type="checkbox"
-                        value="Carpentry"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      ></input>
-                      <label className="font-medium text-gray-700">
-                        Carpenter
-                      </label>
-                      <input
-                        id="plumbing"
-                        name="plumbing"
-                        ref={register}
-                        type="checkbox"
-                        value="Cleaning"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      ></input>
-                      <label className="font-medium text-gray-700">
-                        Cleaning
-                      </label>
-                      <input
-                        id="plumbing"
-                        name="plumbing"
-                        ref={register}
-                        type="checkbox"
-                        value="cooking"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      ></input>
-                      <label className="font-medium text-gray-700">
-                        Chef
-                      </label>
-                    </div>
-                    
+                        <input
+                          id="plumbing"
+                          name="plumbing"
+                          ref={register}
+                          type="checkbox"
+                          value="Driver"
+                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                        ></input>
+                        <label className="font-medium text-gray-700">
+                          Driver
+                        </label>
+                        <input
+                          id="plumbing"
+                          name="plumbing"
+                          ref={register}
+                          type="checkbox"
+                          value="Carpentry"
+                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                        ></input>
+                        <label className="font-medium text-gray-700">
+                          Carpenter
+                        </label>
+                        <input
+                          id="plumbing"
+                          name="plumbing"
+                          ref={register}
+                          type="checkbox"
+                          value="Cleaning"
+                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                        ></input>
+                        <label className="font-medium text-gray-700">
+                          Cleaning
+                        </label>
+                        <input
+                          id="plumbing"
+                          name="plumbing"
+                          ref={register}
+                          type="checkbox"
+                          value="cooking"
+                          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                        ></input>
+                        <label className="font-medium text-gray-700">
+                          Chef
+                        </label>
+                      </div>
 
-                    <button>
                     <input
                       type="submit"
-                      className="w-12 h-12 mr-8 shadow-lg flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700"
+                      className="cursor-pointer h-12 mt-6 shadow-lg flex justify-center px-8 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
                     />
-                  </button>
                   </div>
-                  </form>
-
-                </div>
+                </form>
               </div>
             </div>
           </div>
