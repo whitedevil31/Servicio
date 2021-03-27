fb7bda04fc423198a29d408752a8860a8dd03c03
whitedevil31 <magz3116@gmail.com>
2021-03-27 13:22:03 +0530

    request worker endpoint fix

--- client/src/components/DashboardClient.tsx
+++ client/src/components/DashboardClient.tsx
@@ -4,10 +4,13 @@ import Cookies from "js-cookie";
 import axios, { AxiosRequestConfig } from "axios";
 import NavBar from "./NavBar";
 import { useForm } from "react-hook-form";
+import { isObjectBindingPattern } from "typescript";
 
 function DashboardClient() {
   const history = useHistory();
   const [timeslot, setTimeslot] = useState<any[]>([]);
+  const [selectSlot, setSelectSlot] = useState<any>();
+  const [add, setAdd] = useState<any>();
   const [worker, setWorker] = useState<any[]>([]);
   const [hire, setHireTime] = useState<any[]>([]);
   const [workernearby, setWorkernearby] = useState<any[]>([]);
@@ -28,7 +31,13 @@ function DashboardClient() {
       });
   };
 
-  const timeslotSubmit = (data: any) => {
+  const timeslotSubmit = (e: any) => {
+    e.preventDefault();
+    var slotArray = selectSlot.split("+");
+    const selectedSlot = {
+      start: { startTime: slotArray[0], startFormat: slotArray[1] },
+      end: { endTime: slotArray[2], endFormat: slotArray[3] },
+    };
     var selected = new Array();
     var time = document.getElementById("timeslot");
 
@@ -41,10 +50,6 @@ function DashboardClient() {
         }
       }
     }
-
-    console.log(selected);
-    console.log(data);
-
     let config = {
       withCredentials: true,
       headers: {
@@ -53,17 +58,19 @@ function DashboardClient() {
     };
 
     const timeslotData: any = {
-      slotsSelected: data.time,
+      timeslots: selectedSlot,
       accepted: false,
-
+      workerId: add.workerId,
+      services: add.services,
+      pay: add.pay,
     };
-     console.log(timeslotData)
-    // axios
-    //   .post("http://localhost:5000/api/worker/request", timeslotData, config)
-    //   .then((response) => {
-    //     // console.log(response);
-    //   });
-  }
+
+    axios
+      .post("http://localhost:5000/api/worker/request", timeslotData, config)
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
   useEffect(() => {
     axios
@@ -216,70 +223,78 @@ function DashboardClient() {
 
             <div className="w-1/2 h-full mr-4">
               {worker.map((obj) => (
-                <div
-                  className="flex justify-between cursor-pointer bg-indigo-50 ml-6 mt-8 w-11/12 shadow-inner sm:rounded-2xl border-b-4 
+                <form onSubmit={timeslotSubmit} id="timeslot">
+                  <div
+                    className="flex justify-between cursor-pointer bg-indigo-50 ml-6 mt-8 w-11/12 shadow-inner sm:rounded-2xl border-b-4 
                border-green-800 
                transition duration-300 ease-in-out hover:scale-y-125 hover:bg-indigo-100
                "
-                >
-                  {" "}
-                  <img
-                    className="h-8 w-8 ml-6 mt-7 rounded-b-full"
-                    src="https://images.unsplash.com/photo-1521710696740-c8144a7eaf88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-                    alt=""
-                  ></img>
-                  <h1 className="ml-5 mt-6 mr-5 font-display text-xl text-green-800">
-                    {obj.user.username}
-                  </h1>
-                  <p className="flex-row mt-16 mr-5 -ml-12 font-display text-sm font-bold text-green-800">
-                    Age{obj.user.age}
-                  </p>
-                  <div className="flex-col">
-                    <p className="ml-1 mt-9 mr-2 mb-3 font-display text-mg text-green-800">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      Odit repellat quas velit.
+                  >
+                    {" "}
+                    <img
+                      className="h-8 w-8 ml-6 mt-7 rounded-b-full"
+                      src="https://images.unsplash.com/photo-1521710696740-c8144a7eaf88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                      alt=""
+                    ></img>
+                    <h1 className="ml-5 mt-6 mr-5 font-display text-xl text-green-800">
+                      {obj.user.username}
+                    </h1>
+                    <p className="flex-row mt-16 mr-5 -ml-12 font-display text-sm font-bold text-green-800">
+                      Age{obj.user.age}
                     </p>
-                    <div className="text-xs flex">
-                      {obj.services.map((item: any) => (
-                        <p
-                          className="mt-2 mr-2 bg-gray-700 text-white shadow-2xl p-2 rounded-full text-xs flex-row 
+                    <div className="flex-col">
+                      <p className="ml-1 mt-9 mr-2 mb-3 font-display text-mg text-green-800">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Odit repellat quas velit.
+                      </p>
+                      <div className="text-xs flex">
+                        {obj.services.map((item: any) => (
+                          <p
+                            className="mt-2 mr-2 bg-gray-700 text-white shadow-2xl p-2 rounded-full text-xs flex-row 
                         hover:scale-125"
-                        >
-                          {item}
-                        </p>
-                      ))}
+                          >
+                            {item}
+                          </p>
+                        ))}
+                      </div>
+                      <div className="text-xs flex">
+                        {obj.timeslots.map((time: any) => (
+                          <div className="flex-row mb-2">
+                            <input
+                              className="mr-20"
+                              type="radio"
+                              name="time"
+                              value={`${time.start.startTime} + ${time.start.startFormat} + ${time.end.endTime} + ${time.end.endFormat}`}
+                              onChange={(e) => setSelectSlot(e.target.value)}
+                            />
+                            <div className="flex flex-row bg-gray-400 p-2 rounded-full text-xs">
+                              <p>{time.start.startTime} </p>
+                              <p>{time.start.startFormat}</p> -
+                              <p>{time.end.endTime}</p>
+                              <p>{time.end.endFormat}</p>
+                            </div>
+                          </div>
+                        ))}
+                        <input
+                          type="submit"
+                          onClick={() =>
+                            setAdd({
+                              pay: obj.pay,
+                              services: obj.services,
+                              workerId: obj.user._id,
+                            })
+                          }
+                        />
+                      </div>
                     </div>
-                    <div className="text-xs flex">
-                      <form onSubmit={handleSubmit(timeslotSubmit)} id="timeslot">
-                      
-                       {obj.timeslots.map((time: any) => (
-                         <div className="flex-row mb-2">
-                           <input
-                            className="mr-20"
-                            type="radio"
-                            name="time"
-                            value={`${time.start.startTime} + ${time.start.startFormat} + ${time.end.endTime} + ${time.end.endFormat}`}
-                            ref={register}
-                          />
-                          <div className="flex flex-row bg-gray-400 p-2 rounded-full text-xs">
-                            <p>{time.start.startTime} </p>
-                            <p>{time.start.startFormat}</p> -
-                             <p>{time.end.endTime}</p>
-                             <p>{time.end.endFormat}</p>
-                           </div>
-                         </div>
-                     ))}
-                      <input type="submit" />
-                      </form>
-                    </div>
+                    <a className="ml-2 -mr-10 mt-12 w-12 h-12 px-5 py-5 shadow-lg flex items-center justify-center rounded-full bg-green-300 hover:bg-green-500">
+                      <i className="far fa-comments"></i>
+                    </a>
+                    <p className="flex mt-28 mr-5 font-cursive text-xl text-green-800">
+                      <b>₹{obj.pay}</b>
+                    </p>
                   </div>
-                  <a className="ml-2 -mr-10 mt-12 w-12 h-12 px-5 py-5 shadow-lg flex items-center justify-center rounded-full bg-green-300 hover:bg-green-500">
-                    <i className="far fa-comments"></i>
-                  </a>
-                  <p className="flex mt-28 mr-5 font-cursive text-xl text-green-800">
-                    <b>₹{obj.pay}</b>
-                  </p>
-                </div>
+                </form>
               ))}
             </div>
 
