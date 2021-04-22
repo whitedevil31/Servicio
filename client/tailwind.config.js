de6b43c8a7fee05820dde7db18faa891d3fbcafe
Kaushik Kumaran <kumarankaushik@gmail.com>
2021-04-22 16:29:59 +0400

    font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily : {
-      'display': ['Oswald', 'Roboto']
+      'display': ['Ubuntu', 'Monsterrat']
     }, 
 
     extend: {},
